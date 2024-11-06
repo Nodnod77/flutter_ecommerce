7edb75fa1e124b722d0397b5efbc2d2f0071f98c
Nodnod77 <donia.sakji@etu.imt-nord-europe.fr>
2024-11-06 10:54:37 +0700

    assignement 7

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # flutter_ecommerce
+
+## Assignment Questions
+
+### 1. Explain what are stateless widgets and stateful widgets, and explain the difference between them
+
+In Flutter, widgets are the building blocks of the user interface, and they fall into two main types:
+
+- **Stateless Widgets**: These are immutable widgets, meaning that their state cannot change once created. Stateless widgets are ideal for static content or when the UI does not need to react to user interactions or dynamic changes. Examples include Text, Icon, and Image.
+
+- **Stateful Widgets**: These widgets maintain a mutable state that can change during the widget's lifecycle. Stateful widgets are used when the UI must update dynamically based on user input or other variables. Examples include Checkbox, TextField, and any widget that involves interactivity or animations.
+
+**Difference**: The key difference lies in whether the widget can hold and change its state:
+
+- **Stateless widgets**: No state change, static rendering.
+
+- **Stateful widgets**: Maintains internal state, re-renders upon state changes.
+
+## 2. Mention the widgets that you have used for this project and its uses
+
+Here are some of the widgets used in this project and their purposes:
+
+- **Container**: Used to create rectangular visual elements with customizable properties like padding, margin, and color.
+- **Text**: Displays static text in the interface.
+- **TextField**: Collects user input in the form of text.
+- **ElevatedButton**: Renders a button that the user can tap to trigger actions.
+- **ListView**: Enables vertical or horizontal scrolling of child widgets, often used to display lists of data.
+- **Icon**: Displays an icon to enhance the UI.
+- **Scaffold**: Provides a basic structure, including an AppBar and body, for organizing the app layout.
+
+Each of these widgets contributes to the app’s interface and functionality, helping structure and present the content interactively.
+
+## 3. What is the use-case for setState()? Explain the variable that can be affected by setState()
+
+setState() is a method used in stateful widgets to update the UI in response to state changes. When setState() is called, Flutter marks the widget tree as dirty, triggering a rebuild. This method is commonly used in cases like user interactions, animations, or any situation where the UI needs to react to changes in the state.
+
+Example variable that could be affected by setState():
+
+- **Counter variable** in a counter app, where each button press increments the counter and updates the displayed number.
+- **Boolean variables** controlling visibility or toggling states, like showing or hiding a widget when a button is pressed.
+
+## 4. Explain the difference between const and final keyword.
+
+- **const**: Declares compile-time constants. The value of a const variable must be assigned during compilation and cannot change at runtime. Constants declared with const are deeply immutable.
+
+- **final**: Declares a runtime constant. The value is assigned only once and cannot be reassigned, but it can be determined at runtime. final variables are used for values that might depend on runtime input but should stay constant afterward.
---
+++ e_commerce/lib/menu.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatelessWidget {
+   final String npm = '2406394906 '; // NPM
+    final String name = 'Donia Sakji'; // Name
+    final String className = 'PBP '; // Class
+    MyHomePage({super.key});
+  final List<ItemHomepage> items = [
+         ItemHomepage("View Product List", Icons.mood, Colors.blue),
+         ItemHomepage("Add Product", Icons.add, Colors.red),
+         ItemHomepage("Logout", Icons.logout, Colors.purple),
+     ];
+    @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(  
+        title: const Text(
+          'Mental Health Tracker',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // The background color of the AppBar is obtained from the application theme color scheme.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      // Body of the page with paddings around it.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(child: InfoCard(title: 'NPM', content: npm)),
+              Expanded(child: InfoCard(title: 'Name', content: name)),
+              Expanded(child: InfoCard(title: 'Class', content: className)),
+            ],
+          ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      'Welcome to Mental Health Tracker',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                  GridView.count(
+                    primary: true,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 3,
+                    shrinkWrap: true,
+                    children: items.map((ItemHomepage item) {
+                      return ItemCard(item);
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      ),
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  // Card information that displays the title and content.
+
+  final String title;  // Card title.
+  final String content;  // Card content.
+
+  const InfoCard({super.key, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // Create a card box with a shadow.
+      elevation: 2.0,
+      child: Container(
+        // Set the size and spacing within the card.
+        width: MediaQuery.of(context).size.width / 3.5, // Adjust with the width of the device used.
+        padding: const EdgeInsets.all(16.0),
+        // Place the title and content vertically.
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            Text(content),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemHomepage {
+     final String name;
+     final IconData icon;
+    final Color color; 
+
+     ItemHomepage(this.name, this.icon, this.color);
+ }
+
+
+class ItemCard extends StatelessWidget {
+  // Display the card with an icon and name.
+
+  final ItemHomepage item; 
+  
+  const ItemCard(this.item, {super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      // Specify the background color of the application theme.
+      color: item.color,
+      // Round the card border.
+      borderRadius: BorderRadius.circular(12),
+      
+      child: InkWell(
+        // Action when the card is pressed.
+        onTap: () {
+          // Display the SnackBar message when the card is pressed.
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text("You have pressed the ${item.name} button!"))
+            );
+        },
+        // Container to store the Icon and Text
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              // Place the Icon and Text in the center of the card.
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  
+}
